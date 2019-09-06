212116eaf4b286a00ea10b2cfd9bf120a0fa0999
tarajeanne <t.jeanne.patel@gmail.com>
2019-09-06 09:57:17 -0500

    started endpoints

--- src/server.js
+++ src/server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const cats = require('./stores/cats');
+const dogs = require('./stores/dogs');
 
 const app = express();
 app.use(cors());
 
-// Catch-all 404
+
+app.get('/api/cat', (req, res) => {
+  res.json(cats[0]);
+})
+
+app.get('/api/dog', (req, res) => {
+  res.json(dogs[0]);
+})
+// Catch-all Error handler
+// Add NODE_ENV check to prevent stacktrace leak
 app.use(function (req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
-// Catch-all Error handler
-// Add NODE_ENV check to prevent stacktrace leak
 app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({
